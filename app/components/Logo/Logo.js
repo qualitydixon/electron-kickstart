a74a4625afa14c7136c5b3f94a7e9da288223484
Mike Dixon <qualitydixon@gmail.com>
2016-12-23 20:17:01 -0700

    finish text animation. start logo animation.

--- app/components/Logo/Logo.js
+++ app/components/Logo/Logo.js
@@ -1,4 +1,5 @@
 import React, { Component, PropTypes } from 'react';
+import cx from 'classnames';
 
 require('./Logo.scss');
 
@@ -6,16 +7,22 @@ export default class Logo extends Component {
 	static propTypes = {
 		className: PropTypes.string
 	}
+	constructor(props) {
+		super(props);
+		this.state = {
+			isAnimating: false
+		};
+	}
 	render() {
 		return (
 			<svg className={this.props.className} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 157.89 282.11">
 				<title>Barbershop Logo</title>
 				<g>
-					<polygon style={{ fill: '#c02427'}} points="48.09 282.11 0 257.58 48.09 243.92 48.09 282.11"/>
-					<polygon style={{ fill: '#eb3a28'}} points="157.89 212.75 0 257.58 0 216.7 157.89 171.87 157.89 212.75"/>
-					<polygon style={{ fill: '#c02427'}} points="109.78 37.88 109.78 0 157.89 24.06 109.78 37.88"/>
-					<polygon style={{ fill: '#eb3a28'}} points="157.86 64.94 0 109.77 0 68.89 157.86 24.06 157.86 64.94"/>
-					<polygon style={{ fill: '#eb3a28'}} points="157.89 138.75 0 183.58 0 142.7 157.89 97.87 157.89 138.75"/>
+					<polygon className={cx('poly', {animate: this.state.isAnimating})} style={{ fill: '#c02427'}} points="48.09 282.11 0 257.58 48.09 243.92 48.09 282.11"/>
+					<polygon className={cx('poly', {animate: this.state.isAnimating})} style={{ fill: '#eb3a28'}} points="157.89 212.75 0 257.58 0 216.7 157.89 171.87 157.89 212.75"/>
+					<polygon className={cx('poly', {animate: this.state.isAnimating})} style={{ fill: '#c02427'}} points="109.78 37.88 109.78 0 157.89 24.06 109.78 37.88"/>
+					<polygon className={cx('poly', {animate: this.state.isAnimating})} style={{ fill: '#eb3a28'}} points="157.86 64.94 0 109.77 0 68.89 157.86 24.06 157.86 64.94"/>
+					<polygon className={cx('poly', {animate: this.state.isAnimating})} style={{ fill: '#eb3a28'}} points="157.89 138.75 0 183.58 0 142.7 157.89 97.87 157.89 138.75"/>
 				</g>
 			</svg>
 		);
--- app/components/LogoText/LogoText.js
+++ app/components/LogoText/LogoText.js
@@ -1,4 +1,5 @@
 import React, { Component, PropTypes } from 'react';
+import cx from 'classnames';
 
 require('./LogoText.scss');
 
@@ -6,10 +7,25 @@ export default class Logo extends Component {
 	static propTypes = {
 		className: PropTypes.string
 	}
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			isAnimating: false
+		};
+	}
+	componentDidMount() {
+		setTimeout(() => this.animate(), 2000);
+	}
+	animate() {
+		this.setState({
+			isAnimating: true
+		});
+	}
 	render() {
 		return (
 			<svg className={this.props.className} id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 258.71 40.61">
-				<text className="text" transform="translate(0 30.85)" style={{fontSize: '36px', fontFamily: 'OpenSans'}}>
+				<text className={cx('text', { animate: this.state.isAnimating })} transform="translate(0 30.85)" style={{fontSize: '36px', fontFamily: 'OpenSans'}}>
 					Barbershop I/O
 				</text>
 			</svg>
